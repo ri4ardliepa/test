577eefc8741751d87087c64d6725f58098c64fda
Richard Liepa <81316895+ri4ardliepa@users.noreply.github.com>
2024-01-14 16:02:52 +0200

    test

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Suspense } from "react";
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+
+import Website from "./pages/website";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Website />} />
+            {/*<Route path="/properties" element={<Properties />}/>*/} 
+          </Route>
+        </Routes>  
+      </Suspense>
+    </BrowserRouter>
+  );
+}
+
+export default App;
